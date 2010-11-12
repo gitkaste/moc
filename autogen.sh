553fef49a1731b0758ec60aa93502d1b3837b2e8
Max Klinger <max@fs.lmu.de>
2010-11-12 16:12:50 +0100

    changes to autogen.sh that should help build on macos

--- autogen.sh
+++ autogen.sh
@@ -5,13 +5,18 @@ set -x
 if test -f Makefile; then
 	make distclean >/dev/null 2>/dev/null
 fi
-
-libtoolize -c -f
-aclocal -I m4 && \
-autoheader && \
-automake -W all -a && \
-autoconf -W syntax
-
+if [ `uname` = "Darwin" ]; then
+	glibtoolize -c -f
+	aclocal -I m4 -I /usr/local/share/aclocal
+else
+	libtoolize -c -f
+	aclocal -I m4 
+fi
+if [ "$?" = 0 ]; then
+	autoheader && \
+	automake -W all -a && \
+	autoconf -W syntax
+fi
 if [ "$?" != 0 ]
 then
 	cat <<EOF
