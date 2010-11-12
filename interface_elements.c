43664d2fce34c9e388c0fd7b07d0bb8810ea0efd
Max Klinger <max@fs.lmu.de>
2010-11-12 20:05:08 +0100

    trivial fix

--- interface_elements.c
+++ interface_elements.c
@@ -1857,7 +1857,7 @@ static void main_win_set_dir_content (struct main_win *w,
 	m = find_side_menu (w, iface_to_side_menu(iface_menu));
 
 	side_menu_make_list_content (m, files, dirs, playlists,
-			iface_menu == MENU_PLAYLIST);
+		(enum iface_menu)	iface_menu == MENU_PLAYLIST);
 	if (w->curr_file)
 		side_menu_mark_file (m, w->curr_file);
 	main_win_draw (w);
